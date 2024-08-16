e79d5ec54d9bff8aa94b359e98927d1a0ab2fa12
Dragos-Florin-Pojoga <dragos-florin.pojoga@s.unibuc.ro>
2024-08-16 17:07:26 +0300

    add dumb parser generator

--- encoder/counter.py
+++ encoder/counter.py
@@ -29,3 +29,10 @@ with open('../assembler/instruction_enum.h', 'w') as file:
         file.write(f'\n\t{instruction.replace('.','_')}')
         first = False
     file.write('\n};\n#endif')
+
+with open('../assembler/dumb_parser.hpp', 'w') as file:
+
+    file.write('#ifndef DUMB_PARSER\n#define DUMB_PARSER\n#include "./instruction_enum.h"\n#include <string>\nInstructionType instruction_to_enum(const std::string& instruction){')
+    for instruction in instruction_counter.keys():
+        file.write(f'\n\tif(instruction == "{instruction}"){{return InstructionType::{instruction.replace('.','_')};}}')
+    file.write('\n}\n#endif')
